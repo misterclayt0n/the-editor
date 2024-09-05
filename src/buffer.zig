588067b0ad40d3ebd30f1ab4b979200d95f6a154
misterclayt0n <daviarantes13@gmail.com>
2024-09-05 13:53:41 -0300

    feat: add append buffer methods

--- src/buffer.zig
+++ src/buffer.zig
@@ -1,11 +1,23 @@
 const std = @import("std");
+const stdout = std.io.getStdOut().writer();
 
 pub const AppendBuffer = struct {
     buffer: std.ArrayList(u8),
 
+    /// initialize append buffer
     pub fn init(allocator: *std.mem.Allocator) !AppendBuffer {
         return AppendBuffer{
             .buffer = try std.ArrayList(u8).init(allocator),
         };
+    }
+
+    /// add a string to buffer
+    pub fn append(self: *@This(), data: []const u8) !void {
+        try self.buffer.appendSlice(data);
+    }
+
+    /// free append buffer
+    pub fn free(self: *@This()) void {
+        try self.free();
     }
 };

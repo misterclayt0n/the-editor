c28013fa76c0311356398ce5dcd241efc051e189
misterclayt0n <daviarantes13@gmail.com>
2025-11-24 11:54:03 -0300

    core: add file watcher

--- the-editor/core/file_watcher.rs
+++ the-editor/core/file_watcher.rs
@@ -1,3 +1,8 @@
+use core::slice;
+use std::mem::replace;
+use the_editor_event::dispatch;
+
+use filesentry::Filter;
 use std::{
   borrow::Borrow,
   path::{
@@ -80,6 +85,120 @@ impl Watcher {
 
     watcher.reload(config);
     watcher
+  }
+
+  pub fn reload(&mut self, config: &Config) {
+    let old_config = replace(&mut self.config, config.clone());
+    let (workspace, no_workspace) = the_editor_loader::find_workspace();
+
+    if !config.enable || config.require_workspace && no_workspace {
+      self.watcher = None;
+      return;
+    }
+
+    self.filter = Arc::new(WatchFilter::new(
+      config,
+      &workspace,
+      self.roots.iter().map(|(it, _)| &**it),
+    ));
+
+    let watcher = match &mut self.watcher {
+      Some((watcher, _)) => {
+        // TODO: more fine grained detection of when recrawl is nedded
+        watcher.set_filter(self.filter.clone(), old_config != self.config);
+        watcher
+      },
+      None => {
+        match filesentry::Watcher::new() {
+          Ok(watcher) => {
+            watcher.set_filter(self.filter.clone(), false);
+            watcher.add_handler(move |events| {
+              dispatch(FileSystemDidChange { fs_events: events });
+              true
+            });
+            let shutdown_guard = watcher.shutdown_guard();
+            &mut self.watcher.insert((watcher, shutdown_guard)).0
+          },
+          Err(err) => {
+            log::error!("failed to start file-watcher: {err}");
+            return;
+          },
+        }
+      },
+    };
+
+    if let Err(err) = watcher.add_root(&workspace, true, |_| ()) {
+      log::error!("failed to start file-watcher: {err}");
+    }
+
+    for (root, _) in &self.roots {
+      if let Err(err) = watcher.add_root(root, true, |_| ()) {
+        log::error!("failed to start file-watcher: {err}");
+      }
+    }
+
+    watcher.start();
+  }
+
+  pub fn remove_root(&mut self, root: PathBuf) {
+    let i = self.roots.partition_point(|(it, _)| it < &root);
+
+    if self.roots.get(i).is_none_or(|(it, _)| it != &root) {
+      log::error!("tried to remove root {root:?} from watch list that does not exist!");
+      return;
+    }
+
+    if self.roots[i].1 <= 1 {
+      self.roots.remove(i);
+    } else {
+      self.roots[i].1 -= 1;
+    }
+  }
+
+  pub fn add_root(&mut self, root: &Path) {
+    let root = match root.canonicalize() {
+      Ok(root) => root,
+      Err(err) => {
+        log::error!("failed to watch {root:?}: {err}");
+        return;
+      },
+    };
+    let i = self.roots.partition_point(|(it, _)| it < &root);
+
+    if let Some((_, refcnt)) = self.roots.get_mut(i).filter(|(path, _)| path == &root) {
+      *refcnt += 1;
+      return;
+    }
+
+    if self.roots[..i]
+      .iter()
+      .rev()
+      .find(|(it, _)| it.parent().is_none_or(|it| root.starts_with(it)))
+      .is_some_and(|(it, _)| root.starts_with(it))
+      && !self.filter.ignore_path_rec(&root, Some(true))
+    {
+      return;
+    }
+    let (workspace, _) = the_editor_loader::find_workspace();
+
+    if root.starts_with(&workspace) {
+      return;
+    }
+
+    self.roots.insert(i, (root.clone(), 1));
+    self.filter = Arc::new(WatchFilter::new(
+      &self.config,
+      &workspace,
+      self.roots.iter().map(|(it, _)| &**it),
+    ));
+
+    if let Some((watcher, _)) = &self.watcher {
+      watcher.set_filter(self.filter.clone(), false);
+
+      if let Err(err) = watcher.add_root(&root, true, |_| ()) {
+        log::error!("failed to watch {root:?}: {err}");
+      }
+    }
   }
 }
 
@@ -251,8 +370,91 @@ impl WatchFilter {
       watch_vcs: config.watch_vcs,
     }
   }
+
+  fn ignore_path_impl(
+    &self,
+    path: &Path,
+    is_dir: Option<bool>,
+    ignore_files: &[Arc<Gitignore>],
+  ) -> bool {
+    if let Some(ignore) =
+      IgnoreFiles::is_ignored(slice::from_ref(&self.filesentry_ignores), path, is_dir)
+    {
+      return ignore;
+    }
+
+    if is_hardcoded_whitelist(path) {
+      return false;
+    }
+
+    if is_hardcoded_blacklist(path, is_dir.unwrap_or(false)) {
+      return true;
+    }
+
+    if let Some(ignore) = IgnoreFiles::is_ignored(ignore_files, path, is_dir) {
+      return ignore;
+    }
+
+    // Ignore .git dircectory except .git/HEAD (and .git itself).
+    if is_vcs_ignore(path, self.watch_vcs) {
+      return true;
+    }
+
+    !self.hidden && is_hidden(path)
+  }
 }
 
+impl filesentry::Filter for WatchFilter {
+  fn ignore_path(&self, path: &Path, is_dir: Option<bool>) -> bool {
+    let i = self
+      .ignore_files
+      .partition_point(|ignore_files| path < ignore_files.root);
+
+    let (root, ignore_files) = self
+      .ignore_files
+      .get(i)
+      .map_or((Path::new(""), &self.global_ignores), |files| {
+        (&files.root, &files.ignores)
+      });
+
+    if path == root {
+      return false;
+    }
+
+    self.ignore_path_impl(path, is_dir, ignore_files)
+  }
+
+  fn ignore_path_rec(&self, mut path: &Path, is_dir: Option<bool>) -> bool {
+    let i = self
+      .ignore_files
+      .partition_point(|ignore_files| path < ignore_files.root);
+
+    let (root, ignore_files) = self
+      .ignore_files
+      .get(i)
+      .map_or((Path::new(""), &self.global_ignores), |files| {
+        (&files.root, &files.ignores)
+      });
+
+    loop {
+      if path == root {
+        return false;
+      }
+
+      if self.ignore_path_impl(path, is_dir, ignore_files) {
+        return true;
+      }
+
+      let Some(parent) = path.parent() else {
+        break;
+      };
+
+      path = parent;
+    }
+
+    false
+  }
+}
 // Helpers
 
 fn build_ignore(paths: impl IntoIterator<Item = PathBuf> + Clone, dir: &Path) -> Option<Gitignore> {
@@ -277,5 +479,48 @@ fn build_ignore(paths: impl IntoIterator<Item = PathBuf> + Clone, dir: &Path) ->
 
       None
     },
+  }
+}
+
+fn is_hidden(path: &Path) -> bool {
+  path.file_name().is_some_and(|it| {
+    it.as_encoded_bytes().first() == Some(&b'.')
+        // Handled by vcs ignore rules.
+        && it != ".git"
+  })
+}
+
+// Hidden directories we want to watch by default.
+fn is_hardcoded_whitelist(path: &Path) -> bool {
+  path.ends_with(".the-editor")
+    | path.ends_with(".github")
+    | path.ends_with(".cargo")
+    | path.ends_with(".envrc")
+}
+
+fn is_hardcoded_blacklist(path: &Path, is_dir: bool) -> bool {
+  // Don't descend into the cargo regstiry and similar.
+  path
+    .parent()
+    .is_some_and(|parent| parent.ends_with(".cargo"))
+    && is_dir
+}
+
+fn file_name(path: &Path) -> Option<&str> {
+  path.file_name().and_then(|it| it.to_str())
+}
+
+fn is_vcs_ignore(path: &Path, watch_vcs: bool) -> bool {
+  // Ignore .git dircectory except .git/HEAD (and .git itself).
+  if watch_vcs
+    && path.parent().is_some_and(|it| it.ends_with(".git"))
+    && !path.ends_with(".git/HEAD")
+  {
+    return true;
+  }
+  match file_name(path) {
+    Some(".jj" | ".svn" | ".hg") => true,
+    Some(".git") => !watch_vcs,
+    _ => false,
   }
 }

6ac47e0ffd092af2d94f9d0932c0e1a6ae3bd74c
misterclayt0n <daviarantes13@gmail.com>
2026-02-09 11:55:21 -0300

    ci: make clippy advisory in flake checks

--- flake.nix
+++ flake.nix
@@ -107,7 +107,7 @@
             commonArgs
             // {
               inherit cargoArtifacts;
-              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+              cargoClippyExtraArgs = "--all-targets";
             }
           );
 
